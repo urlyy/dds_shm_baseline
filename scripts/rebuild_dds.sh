186b58cbd70b20c35ee0b02ae21cbac73cb17dc9
urlyy <urlyy@apache.org>
2026-01-30 11:23:26 +0800

    加速rebuild_dds

--- scripts/rebuild_dds.sh
+++ scripts/rebuild_dds.sh
@@ -1,6 +1,7 @@
 IOX_DIR=~/dds_shm/iceoryx
+BUILD_TYPE=Release # Debug or Release
 
 cd ~/dds_shm/cyclonedds
 
-cmake -Bbuild -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=install -DENABLE_ICEORYX=On -DBUILD_EXAMPLES=On -DCMAKE_PREFIX_PATH=$IOX_DIR/install
-cmake --build build --config Release --target install
+cmake -Bbuild -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DCMAKE_INSTALL_PREFIX=install -DENABLE_ICEORYX=On -DBUILD_EXAMPLES=On -DCMAKE_PREFIX_PATH=$IOX_DIR/install
+cmake --build build --config $BUILD_TYPE --target install --parallel $(nproc)
